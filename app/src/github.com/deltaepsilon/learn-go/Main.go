7f483f93273d18406c774f792919892d2e0f84ea
Chris Esplin <chris@christopheresplin.com>
2019-10-05 07:59:36 -0600

    Better deps

--- app/src/github.com/deltaepsilon/learn-go/Main.go
+++ app/src/github.com/deltaepsilon/learn-go/Main.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 	"strconv"
 )
 
 var (
-	i int = 42
+	i int    = 42
 	j string = strconv.Itoa(i + 10)
 )
+
+type Animal struct {
+	Name   string
+	Origin string
+}
 
 func main() {
 
@@ -17,5 +23,50 @@ func main() {
 	// j = strconv.Itoa(i)
 
 	fmt.Printf("%v, %T \n", j, j)
+
+	t := reflect.TypeOf(Animal{})
+	field, _ := t.FieldByName("Name")
+	fmt.Println(field)
+
+	if true {
+		fmt.Println("Hey all")
+	}
+
+	switchTest := 5
+	switch {
+	case switchTest < 10:
+		fmt.Println("we are 10")
+		fallthrough
+	case switchTest < 11:
+		fmt.Println("we are 11")
+		fmt.Println("we are also 11")
+		fallthrough
+	case switchTest < 12:
+		fmt.Println("we are 12")
+	case switchTest > 12:
+		fmt.Printf("We are %v", switchTest)
+	}
+
+	// l := 7
+	// for {
+	// 	fmt.Printf("NOOOO %v\n", l)
+	// 	l++
+
+	// 	if l > 10 {
+	// 		break
+	// 	}
+	// }
+
+Loop:
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			if i*j > 6 {
+				break Loop
+			}
+
+			fmt.Println(i, j)
+
+		}
+	}
 
 }
