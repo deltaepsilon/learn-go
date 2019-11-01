f8d35a6aead62e7ab5a83c4a0fc5e43ab1f70e9f
Chris Esplin <chris@christopheresplin.com>
2019-11-01 06:24:26 -0600

    cipher: local tests passing

--- app/katas/rail-fence-cipher/cipher.go
+++ app/katas/rail-fence-cipher/cipher.go
@@ -2,76 +2,241 @@ package cipher
 
 import (
 	"fmt"
+	"math"
 	"strings"
 )
 
 func Encode(input string, rows int) string {
-	var result string
-	padded := pad(input, rows)
-	length := len(padded)
-	middleRows := rows - 2
-	maxColumn := length/(rows+middleRows) - 1
-	resultSlice := strings.Split(padded, "")
+	length := len(input)
+	lettersPerColumn := 2*rows - 2
+	result := strings.Split(input, "")
+	rowCounts := getRowCounts(input, rows)
 
-	for i, r := range padded {
+	for i, r := range input {
 		var destination int
-		row := i % (rows + middleRows)
-		column := i / (2 + middleRows*2)
+		row, isRising := getRow(i, rows)
+		column := getColumn(i, rows)
 		letter := string(r)
 		isFirstRow := row == 0
-		isLastRow := row == rows-middleRows
-		isRising := row >= rows
-
-		if isRising {
-			row = rows - row + 1
-		}
+		isLastRow := row == lettersPerColumn/2
 
 		if isFirstRow {
 			destination = column
+
 		} else if isLastRow {
-			destination = rows*(maxColumn+1) + column
+			rowCount := rowCounts[row]
+			destination = length - rowCount + column
+
 		} else {
-			destination = row*(maxColumn+1) + column*2
+			countInPrecedingRows := getCountInPrecedingRows(input, i, rows)
+			destination = countInPrecedingRows + column
+
+			if column > 0 {
+				destination += column
+			}
 
 			if isRising {
 				destination++
 			}
 		}
 
-		resultSlice[destination] = letter
+		result[destination] = letter
 	}
 
-	for _, s := range resultSlice {
-		if s != "-" {
-			result += s
+	return strings.Join(result, "")
+}
+
+func Decode(input string, rows int) string {
+	length := len(input)
+	lettersPerColumn := 2*rows - 2
+	columns := int(math.Ceil(float64(length) / float64(lettersPerColumn)))
+	result := strings.Split(input, "")
+
+	fmt.Println("")
+	fmt.Println("input", input)
+	fmt.Println("columns", columns)
+
+	fmt.Println("  i r c d")
+
+	for i, r := range input {
+		var destination int
+		row := getDecodeRow(input, i, rows)
+		column, isRising := getDecodeColumn(input, i, rows)
+		// rowStart := getDecodeRowStart(input, i, rows)
+		isFirstRow := row == 0
+		isLastRow := row == rows-1
+		letter := string(r)
+
+		if isFirstRow {
+			destination = column * lettersPerColumn
+
+		} else if isLastRow {
+			destination = lettersPerColumn/2 + column*lettersPerColumn
+
+		} else {
+			if isRising {
+				intraColumnOffset := (rows-row)*2 - 2
+
+				destination = column*lettersPerColumn + row + intraColumnOffset
+			} else {
+				destination = column*lettersPerColumn + row
+			}
+
+		}
+
+		fmt.Println(letter, i, row, column, destination, isRising)
+
+		result[destination] = letter
+
+	}
+
+	return strings.Join(result, "")
+}
+
+func getDecodeColumn(input string, i, rows int) (int, bool) {
+	var result int
+	var isRising bool
+
+	row := getDecodeRow(input, i, rows)
+	isFirstRow := row == 0
+	isLastRow := row == rows-1
+
+	if isFirstRow {
+		result = i
+	} else if isLastRow {
+		length := len(input)
+		lettersPerColumn := 2*rows - 2
+		fullColumns := length / lettersPerColumn
+		lastRowStart := length - fullColumns
+		result = i - lastRowStart
+	} else {
+		rowStart := getDecodeRowStart(input, i, rows)
+		startIsOdd := rowStart%2 == 1
+		isOdd := i%2 == 1
+		isRising = isOdd != startIsOdd
+		value := i
+
+		if isRising {
+			value--
+		}
+
+		result = (value - rowStart) / 2
+	}
+
+	return result, isRising
+}
+
+func getDecodeRowStart(input string, i, rows int) int {
+	var rowStart int
+	row := getDecodeRow(input, i, rows)
+	rowCounts := getRowCounts(input, rows)
+
+	for i, count := range rowCounts {
+		if i == row {
+			break
+		} else {
+			rowStart += count
 		}
 	}
 
-	fmt.Println("maxColumn, resultSlice", maxColumn, resultSlice)
+	return rowStart
+}
+
+func getDecodeRow(input string, i, rows int) int {
+	var result int = rows - 1
+	var accumulator int
+
+	rowCounts := getRowCounts(input, rows)
+
+	for row, count := range rowCounts {
+		accumulator += count
+
+		if i < accumulator {
+			result = row
+
+			break
+		}
+	}
 
 	return result
 }
 
-func Decode(input string, n int) string {
-	// code
-	return ""
-}
+func getCountInPrecedingRows(input string, i, rows int) int {
+	var result int
+	row, _ := getRow(i, rows)
 
-func pad(input string, rows int) string {
-	result := input
-	length := len(input)
-	middleRows := rows - 2
-	lettersPerColumn := 2 + middleRows*2
-	remainder := length % lettersPerColumn
-	paddedLength := length
+	if row > 0 {
+		rowCounts := getRowCounts(input, rows)
 
-	if remainder > 0 {
-		paddedLength = (length/lettersPerColumn + 1) * lettersPerColumn
-	}
-
-	for i := length; i < paddedLength; i++ {
-		result += "-"
+		for i := 0; i < row; i++ {
+			result += rowCounts[i]
+		}
 	}
 
 	return result
+}
+
+func getCountInPrecedingColumns(input string, i, rows int) int {
+	var result int
+	column := getColumn(i, rows)
+
+	if column > 0 {
+		columnCounts := getColumnCounts(input, rows)
+
+		for i := 0; i < column; i++ {
+			result += columnCounts[i]
+		}
+	}
+
+	return result
+}
+
+func getRowCounts(input string, rows int) []int {
+	var result []int
+
+	for i := 0; i < len(input); i++ {
+		row, _ := getRow(i, rows)
+
+		if len(result) <= row {
+			result = append(result, 0)
+		}
+
+		result[row]++
+	}
+
+	return result
+}
+
+func getColumnCounts(input string, rows int) []int {
+	var result []int
+
+	for i := 0; i < len(input); i++ {
+		column := getColumn(i, rows)
+
+		if len(result) <= column {
+			result = append(result, 0)
+		}
+
+		result[column]++
+	}
+
+	return result
+}
+
+func getRow(i, rows int) (int, bool) {
+	lettersPerColumn := 2*rows - 2
+	row := i % lettersPerColumn
+	isRising := row >= rows
+
+	if isRising {
+		row = lettersPerColumn - row
+	}
+
+	return row, isRising
+}
+
+func getColumn(i, rows int) int {
+	lettersPerColumn := 2*rows - 2
+
+	return i / lettersPerColumn
 }
--- app/katas/rail-fence-cipher/cipher_test.go
+++ app/katas/rail-fence-cipher/cipher_test.go
@@ -9,6 +9,8 @@ import (
 
 var _ = Describe("Cipher", func() {
 	It("Example #1", func() { Expect(Encode("WEAREDISCOVEREDFLEEATONCE", 3)).To(Equal("WECRLTEERDSOEEFEAOCAIVDEN")) })
-	// It("Example #2", func() { Expect(Decode("WECRLTEERDSOEEFEAOCAIVDEN", 3)).To(Equal("WEAREDISCOVEREDFLEEATONCE")) })
-	// It("Example #3", func() { Expect(Encode("Hello, World!", 3)).To(Equal("Hoo!el,Wrdl l")) })
+	It("Example #1 with four columns", func() { Expect(Encode("WEAREDISCOVEREDFLEEATONCE!", 4)).To(Equal("WIREEEDSEEEAC!AECVDLTNROFO")) })
+	It("Example #2 with four columns", func() { Expect(Decode("WIREEEDSEEEAC!AECVDLTNROFO", 4)).To(Equal("WEAREDISCOVEREDFLEEATONCE!")) })
+	It("Example #2", func() { Expect(Decode("WECRLTEERDSOEEFEAOCAIVDEN", 3)).To(Equal("WEAREDISCOVEREDFLEEATONCE")) })
+	It("Example #3", func() { Expect(Encode("Hello, World!", 3)).To(Equal("Hoo!el,Wrdl l")) })
 })
