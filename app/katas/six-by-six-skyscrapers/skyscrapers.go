14256f9e198851c6bd9d2f480f9b3e969d22c961
Chris Esplin <chris@christopheresplin.com>
2019-11-06 08:18:08 -0700

    Solving cells by deduction

--- app/katas/six-by-six-skyscrapers/skyscrapers.go
+++ app/katas/six-by-six-skyscrapers/skyscrapers.go
@@ -21,10 +21,10 @@ func SolvePuzzle(clues []int) [][]int {
 			eMap = sixes(eMap, clue, i)
 			logMap(eMap)
 
-		// case 5:
-		// 	fmt.Println("fives")
-		// 	eMap = fives(eMap, clue, i)
-		// 	logMap(eMap)
+		case 5:
+			fmt.Println("fives")
+			eMap = fives(eMap, clue, i)
+			logMap(eMap)
 
 		case 4:
 			fmt.Println("fours")
@@ -41,10 +41,10 @@ func SolvePuzzle(clues []int) [][]int {
 			eMap = twos(eMap, clue, i)
 			logMap(eMap)
 
-			// case 1:
-			// 	fmt.Println("ones")
-			// 	eMap = ones(eMap, clue, i)
-			// 	logMap(eMap)
+		case 1:
+			fmt.Println("ones")
+			eMap = ones(eMap, clue, i)
+			logMap(eMap)
 
 		}
 	}
@@ -75,6 +75,11 @@ func sixes(eMap eliminationMap, clue, clueNumber int) eliminationMap {
 
 func fives(eMap eliminationMap, clue, clueNumber int) eliminationMap {
 	return processClue(eMap, clue, clueNumber, func(v vector, clue int) vector {
+		v[0].values[5] = false
+		v[1].values[5] = false
+		v[2].values[5] = false
+		v[3].values[5] = false
+
 		return v
 	})
 }
@@ -108,6 +113,8 @@ func twos(eMap eliminationMap, clue, clueNumber int) eliminationMap {
 
 func ones(eMap eliminationMap, clue, clueNumber int) eliminationMap {
 	return processClue(eMap, clue, clueNumber, func(v vector, clue int) vector {
+		v[0].values = [6]bool{false, false, false, false, false, true}
+
 		return v
 	})
 }
@@ -120,54 +127,29 @@ func processClue(eMap eliminationMap, clue, clueNumber int, processVector func(v
 
 	switch clueBlock {
 	case 0:
-		for i := 0; i < 6; i++ {
-			for j := 0; j < 6; j++ {
-				if j == vectorNumber {
-					v[i] = eMap[i][j]
-				}
-			}
-		}
-
+		v = getColumn(eMap, vectorNumber)
 		v = processVector(v, clue)
 		processed = true
-	case 1:
-		for i, cell := range eMap[vectorNumber] {
-			v[5-i] = cell
-		}
 
+	case 1:
+		v = getReversedRow(eMap, vectorNumber)
 		v = processVector(v, clue)
 		processed = true
 
 	case 2:
-		for i := 0; i < 6; i++ {
-			for j := 0; j < 6; j++ {
-				if j == 5-vectorNumber {
-					v[5-i] = eMap[i][j]
-				}
-			}
-		}
-
+		v = getReversedColumn(eMap, 5-vectorNumber)
 		v = processVector(v, clue)
 		processed = true
 
 	case 3:
-		for i := 0; i < 6; i++ {
-			for j := 0; j < 6; j++ {
-				if i == 5-vectorNumber {
-					v[j] = eMap[i][j]
-				}
-			}
-		}
-
+		v = getRow(eMap, 5-vectorNumber)
 		v = processVector(v, clue)
 		processed = true
+
 	}
 
 	if processed {
-		for _, cell := range v {
-
-			eMap[cell.row][cell.column] = cell
-		}
+		eMap = replaceVector(eMap, v)
 	}
 
 	fmt.Println("")
@@ -179,26 +161,11 @@ func processClue(eMap eliminationMap, clue, clueNumber int, processVector func(v
 }
 
 func eliminate(eMap eliminationMap) eliminationMap {
-	result := eMap
+	result := solveMap(eMap)
 
 	for rowNumber := 0; rowNumber < 6; rowNumber++ {
 		for columnNumber := 0; columnNumber < 6; columnNumber++ {
-			c := result[rowNumber][columnNumber]
-			solution := getSolution(c)
-
-			if solution != -1 {
-				result = processEliminationMap(result, func(c cell) cell {
-					if c.row == rowNumber && c.column != columnNumber {
-						c.values[solution] = false
-					}
-
-					if c.column == columnNumber && c.row != rowNumber {
-						c.values[solution] = false
-					}
-
-					return c
-				})
-			}
+			result = eliminateSolvedCell(result, rowNumber, columnNumber)
 		}
 	}
 
@@ -207,6 +174,77 @@ func eliminate(eMap eliminationMap) eliminationMap {
 	}
 
 	return result
+}
+
+func solveMap(eMap eliminationMap) eliminationMap {
+	for i := 0; i < 6; i++ {
+		row := getRow(eMap, i)
+		column := getColumn(eMap, i)
+
+		eMap = replaceVector(eMap, solveVector(row))
+		eMap = replaceVector(eMap, solveVector(column))
+	}
+
+	return eMap
+}
+
+func solveVector(v vector) vector {
+	var frequencies [6]int
+
+	for _, cell := range v {
+		for i, value := range cell.values {
+			if value {
+				frequencies[i]++
+			}
+		}
+	}
+
+	for i, frequency := range frequencies {
+		if frequency == 1 {
+			for j := 0; j < 6; j++ {
+				cell := v[j]
+
+				if cell.values[i] == true {
+					for k := 0; k < 6; k++ {
+						if i != k {
+							v[j].values[k] = false
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return v
+}
+
+func replaceVector(eMap eliminationMap, v vector) eliminationMap {
+	for _, cell := range v {
+		eMap[cell.row][cell.column] = cell
+	}
+
+	return eMap
+}
+
+func eliminateSolvedCell(eMap eliminationMap, rowNumber, columnNumber int) eliminationMap {
+	c := eMap[rowNumber][columnNumber]
+	solution := getSolution(c)
+
+	if solution != -1 {
+		eMap = processEliminationMap(eMap, func(c cell) cell {
+			if c.row == rowNumber && c.column != columnNumber {
+				c.values[solution] = false
+			}
+
+			if c.column == columnNumber && c.row != rowNumber {
+				c.values[solution] = false
+			}
+
+			return c
+		})
+	}
+
+	return eMap
 }
 
 func getSolution(c cell) int {
@@ -239,6 +277,54 @@ func processEliminationMap(eMap eliminationMap, processCell func(c cell) cell) e
 
 func hasChanged(a, b eliminationMap) bool {
 	return fmt.Sprint(a) != fmt.Sprint(b)
+}
+
+func getColumn(eMap eliminationMap, columnNumber int) vector {
+	var v vector
+
+	for i := 0; i < 6; i++ {
+		for j := 0; j < 6; j++ {
+			if j == columnNumber {
+				v[i] = eMap[i][j]
+			}
+		}
+	}
+
+	return v
+}
+
+func getReversedColumn(eMap eliminationMap, columnNumber int) vector {
+	var v vector
+
+	for i := 0; i < 6; i++ {
+		for j := 0; j < 6; j++ {
+			if j == columnNumber {
+				v[5-i] = eMap[i][j]
+			}
+		}
+	}
+
+	return v
+}
+
+func getRow(eMap eliminationMap, rowNumber int) vector {
+	var v vector
+
+	for i, cell := range eMap[rowNumber] {
+		v[i] = cell
+	}
+
+	return v
+}
+
+func getReversedRow(eMap eliminationMap, rowNumber int) vector {
+	var v vector
+
+	for i, cell := range eMap[rowNumber] {
+		v[5-i] = cell
+	}
+
+	return v
 }
 
 // Logging
